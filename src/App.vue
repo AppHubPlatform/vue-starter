8e8ab740019aeae3f08b78bca0d6a2932736af34
Ben Edelstein <ben.edelstein93@gmail.com>
2017-12-22 13:42:47 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Paginator/>
+  </div>
+</template>
+
+<script>
+import Paginator from './components/Paginator'
+
+export default {
+  name: 'app',
+  components: {
+    Paginator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Paginator.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="box-container">
+    <div class="box">
+      <i class="fa fa-chevron-left"></i>
+    </div>
+    <div class="box" v-for="page in pages">
+      {{page}}
+    </div>
+    <div class="box">
+      <i class="fa fa-chevron-right"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Paginator',
+  data () {
+    return {
+      pages: [1, 2, 3, 4, 5],
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .box-container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .box {
+    width: 25px;
+    height: 25px;
+    border: 1px solid black;
+    cursor: pointer;
+    margin-right: 2px;
+  }
+</style>
